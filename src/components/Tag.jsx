c5a1988a9a6d71db7b634ff6963baac043d55c42
AzizBHY <62714614+Aziz-BHY@users.noreply.github.com>
2022-08-20 22:34:25 +0100

    search pods

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,11 +1,12 @@
 import { PlusIcon } from '@heroicons/react/outline'
 import React from 'react'
 
-export const Tag = ({ title }) => {
+export const Tag = ({ title, actif, setActif }) => {
     return (
-        <div className='rounded-full cursor-pointer flex justify-between items-center px-6 border border-gris3 bg-neutral-300 py-2 space-x-4 w-44'>
+        <div onClick={setActif} className='rounded-full cursor-pointer flex justify-between items-center px-6 border border-gris3 bg-neutral-300 py-2 space-x-4 w-44' style={actif?{backgroundColor: "#FFC62D"}: {}}>
             <div className='text-lg text-gris3'>{title}</div>
             <PlusIcon className='h-4 w-4 text-gris3' />
         </div>
     )
 }
+
